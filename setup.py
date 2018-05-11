8d4b19994437516221cf82501e835b4443779a48
Nate Tangsurat <e4r7hbug@gmail.com>
2018-05-11 10:03:41 -0500

    fix: Use setuptools_scm for versioning

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(name='git_update',
-      version='1.0',
       description='Update several Git repositories in a directory.',
       author='Nate Tangsurat',
       author_email='e4r7hbug@gmail.com',
       packages=find_packages(),
+      setup_requires=['setuptools_scm'],
+      use_scm_version=True,
       install_requires=['click',
                         'gitpython', ],
       keywords='git update',
